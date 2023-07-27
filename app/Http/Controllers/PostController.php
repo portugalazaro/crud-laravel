d503af6d351a7c993633bdf8f40cba97443e6b87
Lazaro Portugal <portugallazaro@gmail.com>
2023-07-27 14:45:03 -0300

    Criando o metodo CREATE,  dentro da classe PostController e alterando a rota:
    /create para /post/create

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
     //
+
+    public function create(Request $request)
+    {
+        $dados = [
+            'title' => 'Primeiro Post',
+            'content' => 'meu conteudo do primeiro post...',
+            'author'  => 'Lázaro Portugal'
+        ];
+
+        // modo mais convencional de salvar os dados no banco, passando direto para o
+        // construtor do model POST
+        // $post = new Post($dados);
+        // $post->save();
+        // dd($post);
+
+        // ------------------------------------------
+
+        // Passando cada valor para seu dado
+        $post = new Post();
+        $post->title = 'Segudo post';
+        $post->content = 'Conteudo segundo post...';
+        $post->author = 'Savio Portugal';
+        $post->save();
+
+    }
 }
