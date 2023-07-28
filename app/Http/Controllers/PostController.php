14e2c102ecfa179488cabf95a64806d1cefca217
Lazaro Portugal <portugallazaro@gmail.com>
2023-07-28 20:36:22 -0300

    criação de todas as rotas e funcionalidades do api
    metodos
    
    create
    read
    update
    delete
    
    e suas respectivas rotas

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,9 +12,9 @@ class PostController extends Controller
     public function create(Request $request)
     {
         $dados = [
-            'title' => 'Primeiro Post',
+            'title' => 'Post para Testes',
             'content' => 'meu conteudo do primeiro post...',
-            'author'  => 'Lázaro Portugal'
+            'author'  => 'Desconhecido'
         ];
 
         // modo mais convencional de salvar os dados no banco, passando direto para o
@@ -26,11 +26,90 @@ class PostController extends Controller
         // ------------------------------------------
 
         // Passando cada valor para seu dado
-        $post = new Post();
-        $post->title = 'Segudo post';
-        $post->content = 'Conteudo segundo post...';
-        $post->author = 'Savio Portugal';
-        $post->save();
+        $post = new Post($dados);
+        // $post->title = 'Segudo post';
+        // $post->content = 'Conteudo segundo post...';
+        // $post->author = 'Savio Portugal';
+        return $post->save();
 
+    }
+
+
+    public function read(Request $request)
+    {
+        // all() -> retorna todos os dados da tabela
+        // find() -> retorna somente 1 dado, que é buscado pela sua primary key
+
+
+        // verificando se id existe e se é diferente de vazio
+        if(isset($request['id']) && $request['id'] != '') {
+
+            // verificando se o post existe no banco de dados
+            if(Post::find($request['id'])) {
+                return Post::find($request['id']);
+            }
+        }
+        
+        return false;
+    }
+
+
+    public function all(Request $request)
+    {
+        // retornando todos os post
+        return $post = Post::all();
+    } 
+
+
+
+    public function update(Request $request)
+    {
+        // pegando o post pelo id
+        // $post =  $this->read($request);
+
+        // Forma 1 de atualizar dados no banco
+        // $post->title = 'testando';
+        // $post->author = 'lazinho';
+        // $post->content = 'conteudo';
+        // $post->save();
+        // -----------------------------------------------
+        
+        
+        // forma 2 de atualizar um ou varios registros no banco de dados
+        // Dessa forma nao precisa chamar o metodo save, o metodo update já salva no banco
+
+
+        $post = Post::where('id', '>' , 0)->update([
+            'title' => 'Atualizando post',
+            'author' => 'Maquina',
+            'content' => rand(1,100)
+        ]);
+
+        return $post;
+
+
+    }
+
+
+    public function delete(Request $request)
+    {
+        // pegando o post valido
+        $post = $this->read($request); 
+
+        // verificando se o post é valido, se for, deltar. caso contrário retorna false
+        return $post ? $post->delete($post->id) : false;
+    }
+
+
+    public function deleteAll(Request $request)
+    {
+        $post = Post::where('id', '>', 0)->delete();
+        return 'deu liga';
+    }
+
+
+    public function index(Request $request)
+    {
+        return view('welcome');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,25 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
+/*
+    CRUD -> web api
+
+    CREATE -> CRIAR DADOS ( X  )
+    READ  -> LER DADOS ( x )
+    UPDATE -> ATUALIZAR DADOS ( x )
+    DELETE -> DELETAR DADOS ( x )
+*/ 
+
 
 Route::get('/post/create', [PostController::class, 'create']);
+Route::get('/post/read/{id}', [PostController::class, 'read']);
+Route::get('/post/all', [PostController::class, 'all']);
+Route::get('/post/update/{id?}', [PostController::class, 'update']);
+Route::get('/post/delete/all', [PostController::class, 'deleteAll']);
+Route::get('/post/delete/{id?}', [PostController::class, 'delete']);
+
+// o parametro {id?} ponto de interrogação na frente, torna o parametro opcional
+
+
+Route::get('/teste', [PostController::class, 'index']);
 
